77ad237271172ca82970032f2db9bfe9dfddf000
Zuhairi Rahmad <zuhairi.bit@gmail.com>
2018-12-02 17:12:31 +0900

    First Commit

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+	void Update () {
+		 transform.Rotate (new Vector3 (13, 30, 45) * Time.deltaTime);		 		
+	}
+}
